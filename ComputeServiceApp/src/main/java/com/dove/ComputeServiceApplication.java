a80adfef00271cd5b6776f48487f860a3f5e1b9a
ytl <993442230@qq.com>
2017-05-13 16:31:15 +0800

    main method

--- ComputeServiceApp/src/main/java/com/dove/ComputeServiceApplication.java
+++ ComputeServiceApp/src/main/java/com/dove/ComputeServiceApplication.java
@@ -1,8 +1,18 @@
 package com.dove;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * Created by jh on 2017/5/13.
  */
+@EnableDiscoveryClient
+@SpringBootApplication
 public class ComputeServiceApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ComputeServiceApplication.class).web(true).run(args);
+	}
 
 }
