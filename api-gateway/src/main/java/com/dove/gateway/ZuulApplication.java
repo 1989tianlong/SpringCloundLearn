4cc2857e1ed7396e5e5a5cb5c69d1ad54563246d
ytl <993442230@qq.com>
2017-05-15 16:22:31 +0800

    add api-gateway

---
+++ api-gateway/src/main/java/com/dove/gateway/ZuulApplication.java
@@ -0,0 +1,26 @@
+package com.dove.gateway;
+
+import com.dove.filter.AccessFilter;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Created by jh on 2017/5/15.
+ */
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
+	}
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
+	}
+
+}
